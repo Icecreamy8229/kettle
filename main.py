d50a5ac4a34d99926f9ce0dffcf685a71fa00319
Icecreamy8229 <lolbdicey@gmail.com>
2025-02-01 13:26:26 -0500

    added config file.
    added config.yaml and the code that loads it to main.py
    added to the requirements.txt file
    added the ignore for the config to .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from routes import routes
+import yaml
+
 
 #initializes our app and tells it where to look for html pages.
 app = Flask(__name__, template_folder='templates')
@@ -8,7 +10,16 @@ app = Flask(__name__, template_folder='templates')
 app.register_blueprint(routes)
 
 
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
 #this if statement just checks that you are running main.py, rather than an import
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    if config['environment'] == 'production':
+        app.run(debug=False)
+    else:
+        app.run(debug=True)
+
+
+
