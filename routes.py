d2ee83b152787e31f02802bed634ab673bb50d8b
Icecreamy8229 <lolbdicey@gmail.com>
2025-02-17 23:52:32 -0500

    created base.html modified index.html for the purpose of demonstrating jinja moved brendans styling to styles.css

--- routes.py
+++ routes.py
@@ -1,9 +1,13 @@
 from flask import render_template, Blueprint
 import logging
+import random
 
 routes = Blueprint('routes', __name__) #this module points to itself for routes.
 
 @routes.route('/') #This is the general syntax for creating a route in flask.
 def index():
     logging.debug('Index route called')
-    return render_template('index.html')
+    random_number = random.randint(0, 9)  #we can remove, just showing an example of template tags.
+    return render_template('index.html', random_number=str(random_number))
+
+
