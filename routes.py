b0a0fd32956ef540f2c01045d7019f29fac31ecb
Icecreamy8229 <lolbdicey@gmail.com>
2025-02-18 23:35:47 -0500

    Revert "additional routes created"
    This reverts commit 1c088b5c1ae8d690f52e2a7d65f34a3a4fdc202f.

--- routes.py
+++ routes.py
@@ -5,19 +5,9 @@ import random
 routes = Blueprint('routes', __name__) #this module points to itself for routes.
 
 @routes.route('/') #This is the general syntax for creating a route in flask.
-def index_route():
+def index():
     logging.debug('Index route called')
     random_number = random.randint(0, 9)  #we can remove, just showing an example of template tags.
     return render_template('index.html', random_number=str(random_number))
 
 
-@routes.route('/game') #this will be modified later, but adding so we can hit the game page url for now.
-def game_route():
-    logging.debug('Game route called')
-    return render_template('game.html')
-
-
-@routes.route('/user')
-def user_route():
-    logging.debug('User route called')
-    return render_template('user.html')
