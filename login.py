56218e742b29e1033a1ffc3422ebe60eb9478955
Icecreamy8229 <lolbdicey@gmail.com>
2025-04-09 22:04:11 -0400

    implemented savannahs bugfix suggestion to handle 401's differently
    401's will now redirect the user to the login page and flash a message to login first.

--- login.py
+++ login.py
@@ -1,8 +1,20 @@
 from flask_login import LoginManager
 from models import db, User
+from flask import redirect, url_for, flash
 login_manager = LoginManager()
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.get(User, int(user_id))
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """
+    This is how all unauthorized users will be handled.
+    redirected to the login page.
+    :return:
+    """
+    flash("please login first", "danger")
+    return redirect(url_for("routes.login_route"))
